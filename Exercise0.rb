1c0fb75f9c05c7e15bfe5ad8090db11a90e718d4
EDWIN AU <edwinau@rogers.com>
2017-03-29 15:11:29 -0400

    Ex0 complete

---
+++ Exercise0.rb
@@ -0,0 +1,21 @@
+#Ex0.1
+fav_colours = [blue, red, black, green, yellow]
+ourAges {
+  :Me => 26,
+  :David => 31,
+  :Yonge => 23
+  :Greg => 48
+}
+coinFlip {
+  :first => "heads"
+  :second => "tails"
+  :third => "heads"
+  :fourth => "heads"
+  :fifth => "tails"
+
+#Ex0.2
+wordsDef {
+  :dog => "A canine."
+  :love => "A feeling."
+  :hat => "An accessory a person wears."
+}
