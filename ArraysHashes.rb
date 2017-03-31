7606d672e89a86cc400e70bcf646c83dc539d660
EDWIN AU <edwinau@rogers.com>
2017-03-31 00:57:41 -0400

    Ex6 complete

--- ArraysHashes.rb
+++ ArraysHashes.rb
@@ -8,6 +8,7 @@ coinFlip = ["heads","heads","heads","heads","tails"]
 performingArtists = ["Ed Sheeran", "Sam Smith", "Adele"]
 
 fav_colours = [:blue, :red, :black, :green, :yellow]
+fav_colours2 = ["blue", "red", "black", "green", "yellow"]
 
 #Ex0.2
 wordsDef = {
@@ -34,82 +35,83 @@ myCircle = {
   :mother => 50
 }
 
-# #Ex1
-# puts "Exercise 1"
-# puts "1. " + coinFlip.to_s
-# puts "2. " + fav_colours.first.to_s
-# puts "3a." + ourAges.sort.to_s
-# puts "4. " + ourAges.to_s << ["baby",0].to_s
-# puts "5. " + "{movies[:AI]}"
-#
-#Ex2.
-# puts "1"
-# puts fav_colours.last
-# puts " "
-#
-# puts "2"
-# puts population[:key] = "Milan"
-# puts " "
-#
-# puts "3"
-# puts coinFlip.reverse!
-# puts " "
-#
-# puts "4"
-# puts population [:london]
-# puts " "
-#
-# puts "5"
-# performingArtists.each do |x|
-#   puts "I think #{x} is great."
-# end
-#
-# Ex3.
-# puts "1"
-# puts performingArtists[1,2]
-# puts " "
-#
-# puts "2"
-# movies.each do |x,y|
-#   puts "#{x} came out in #{y}."
-# end
-# puts " "
-#
-# puts "3a"
-# puts ourAges.sort.reverse {|a| a(0) <=> a(0)}
-# puts " "
-#
-# puts "4"
-# movies [:Beauty_and_the_Beast] = 1997 , 2017
-# puts movies
-# puts " "
+#Ex1
+puts "Exercise 1"
+puts "1. " + coinFlip.to_s
+puts "2. " + fav_colours.first.to_s
+puts "3a." + ourAges.sort.to_s
+puts "4. " + ourAges.to_s << ["baby",0].to_s
+puts "5. " + "{movies[:AI]}"
 
-#Ex4.
-# puts "1."
-# puts ourAges.select { |k,v| v < 31 }
-# puts " "
-#
-# puts "2."
-# puts ourAges.max
-# puts " "
-#
-# puts "3."
-# puts coinFlip.count "heads"
-# puts " "
-#
-# puts "4."
-# performingArtists.delete("Adele")
-# puts performingArtists
-# puts " "
-#
-# puts "5."
-# population[:Paris] = "3.2 Million"
-# puts population
+# Ex2.
+puts "1"
+puts fav_colours.last
+puts " "
+
+puts "2"
+puts population[:key] = "Milan"
+puts " "
+
+puts "3"
+puts coinFlip.reverse!
+puts " "
+
+puts "4"
+puts population [:london]
+puts " "
+
+puts "5"
+performingArtists.each do |x|
+  puts "I think #{x} is great."
+end
+
+# Ex3.
+puts "1"
+puts performingArtists[1,2]
+puts " "
+
+puts "2"
+movies.each do |x,y|
+  puts "#{x} came out in #{y}."
+end
+puts " "
+
+puts "3a"
+puts ourAges.sort.reverse {|a| a(0) <=> a(0)}
+puts " "
+
+puts "4"
+movies [:Beauty_and_the_Beast] = 1997 , 2017
+puts movies
+puts " "
+
+# Ex4.
+puts "1."
+puts ourAges.select { |k,v| v < 31 }
+puts " "
+
+puts "2."
+puts ourAges.max
+puts " "
+
+puts "3."
+puts coinFlip.count "heads"
+puts " "
+
+puts "4."
+performingArtists.delete("Adele")
+puts performingArtists
+puts " "
+
+puts "5."
+population[:Paris] = "3.2 Million"
+puts population
+
 # Ex5.
 puts "1."
 sum = 0
 population.each do |k,v|
-  sum += v
+  sum += v.to_f
 end
 puts "total population is #{sum}"
 puts " "
@@ -137,3 +139,25 @@ end
 puts "5."
 fav_colours << "fuschia" << "cobalt"
 puts fav_colours
+
+#Ex6a
+puts "1."
+newMovies = {
+ :"1999" => ["The_Matrix", "Beauty_and_the_Beast", "The Mommy"],
+ :"2009" => ["Avatar", "Star Trek", "District 9"]
+}
+newMovies.each do |k,v|
+    puts v
+end
+
+# # puts "2."
+# # # puts phoneRows = [[1,2,3],[4,5,6],[7,8,9,] [*,0,c]]
+# #
+# # puts "3"
+#
+# # Ex7
+# var = fav_colours2
+#
+# var = var << performingArtists
+# var.class
+# puts var.sort {|x,y| x <=> y }
